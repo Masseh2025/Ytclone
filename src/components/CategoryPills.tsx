307f63ae5c36cdd42cb37a213973f40fe3b54a44
Masseh Ziaie <mas154572@gmail.com>
2025-01-20 06:57:07 -0500

    added  left button and did work on categories

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,6 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { Button } from "./Button";
+import { useRef, useState } from "react";
 
 type CategoryPillProps = {
   categories: string[];
@@ -6,14 +8,24 @@ type CategoryPillProps = {
   onSelect: (category: string) => void;
 };
 
+const TRANSLATE_AMOUNT = 200;
+
 export function CategoryPills({
   categories,
   selectedCategory,
   onSelect,
 }: CategoryPillProps) {
+  const [translate, setTranslate] = useState(0);
+  const [isLeftVisible, setIsLeftVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
   return (
     <div className="overflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-max">
+      <div
+        ref={containerRef}
+        className="flex whitespace-nowrap gap-3 transition-transform w-max"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map((category) => (
           <Button
             key={category}
@@ -25,6 +37,38 @@ export function CategoryPills({
           </Button>
         ))}
       </div>
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            varient="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() =>
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_AMOUNT;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              })
+            }
+          >
+            <ChevronLeftIcon />
+          </Button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+          <Button
+            varient="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              console.log("hi");
+            }}
+          >
+            <ChevronRightIcon />
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
