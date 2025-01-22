a8a566affcf48d340e781e1a9c7c5dfb1df817b3
Masseh Ziaie <mas154572@gmail.com>
2025-01-22 06:17:44 -0500

    added icons to sidebar

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,3 +1,40 @@
-export default function SideBar() {
-  return <div>SideBar</div>;
+import { Clapperboard, Home, Library, Repeat } from "lucide-react";
+import { ElementType } from "react";
+import { buttonStyles } from "../components/Button";
+import { twMerge } from "tailwind-merge";
+
+export function Sidebar() {
+  return (
+    <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+      <SmallSideBarItem Icon={Home} title="Home" url="/" />
+      <SmallSideBarItem Icon={Repeat} title="Shorts" url="/shorts" />
+      <SmallSideBarItem
+        Icon={Clapperboard}
+        title="Subscriptions"
+        url="/subscriptions"
+      />
+      <SmallSideBarItem Icon={Library} title="Library" url="/library" />
+    </aside>
+  );
+}
+
+type SmallSideBarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+};
+
+function SmallSideBarItem({ Icon, title, url }: SmallSideBarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ varient: "ghost" }),
+        "py-4 px-1 flex flex-col items-center rounded-lg gap-1"
+      )}
+    >
+      <Icon className="w-6 h-6" />
+      <div className="text-sm">{title}</div>
+    </a>
+  );
 }
