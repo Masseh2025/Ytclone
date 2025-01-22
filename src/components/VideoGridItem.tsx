9fecc3ed80fa41ee57cfbea7c0e58424d6d5c2a2
Masseh Ziaie <mas154572@gmail.com>
2025-01-22 05:46:36 -0500

    ifinished interactivity for the video grid started working on sidebar

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { formatDuration } from "../utils/formatDuration";
 import { formatTimeAgo } from "../utils/formatTimeAgo";
 
@@ -28,16 +29,49 @@ export function VideoGridItem({
   thumbnailUrl,
   videoUrl,
 }: VideoGridItemProps) {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current === null) return;
+
+    if (isVideoPlaying) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }, [isVideoPlaying]);
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2"
+      onMouseOver={() => setIsVideoPlaying(true)}
+      onMouseLeave={() => setIsVideoPlaying(false)}
+    >
       <a href={`/watch?v=${id}`} className="relative aspect-video">
         <img
           src={thumbnailUrl}
-          className="block w-full h-full object-cover rounded-xl"
+          className={
+            isVideoPlaying
+              ? `block w-full h-full object-cover rounded-none transition-[border-raidus] duration-200`
+              : `block w-full h-full object-cover rounded-xl transition-[border-raidus] duration-200`
+          }
         />
         <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
           {formatDuration(duration)}
         </div>
+        <video
+          className={
+            isVideoPlaying
+              ? `block h-full object-cover absolute inset-0 transition-opacity duration-200 opacity-100 delay-200`
+              : `block h-full object-cover absolute inset-0 transition-opacity duration-200 opacity-0 `
+          }
+          ref={videoRef}
+          muted
+          playsInline
+          src={videoUrl}
+        />
       </a>
       <div className="flex gap-2">
         <a href={`/@${channel.id}`} className="flex-shrink-0">
